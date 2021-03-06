56ffd6dfc0dea042d69b72d3e5c8b0c8b55e3acc
parsa <parsa.eskandar@gmail.com>
2021-03-06 17:30:25 +0330

    train and fit
    learn from data and fit random forest model

--- random_forest.py
+++ random_forest.py
@@ -15,3 +15,20 @@ def random_forest(mode="combined"):
             arr_train.append(elem)
 
     array = np.array(arr_train)
+    x_train = np.reshape(array, (len(df_train[mode]), len(df_train[mode][0])))
+    y_train = df_train["views"]
+    rfc = RandomForestClassifier()
+    rfc.fit(x_train, y_train)
+
+    df_test = read_test_data(path_src)
+    df_test[mode] = df_test[mode].apply(lambda x: sparce_matrix_to_list(x))
+    arr_test = []
+
+    arr = np.array(arr_test)
+    x_test = np.reshape(arr, (len(df_test[mode]), len(df_test[mode][0])))
+    y_test = df_test["views"]
+    y_pred = rfc.predict(x_test)
+    # acc_calc(y_pred, y_test)
+    # print(precision_recall_fscore_support(y_test, y_pred))
+    print(y_pred)
+    return y_pred
