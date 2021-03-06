b908f4ee8713045ca5e33316f1eb5275cb7063bd
parsa <parsa.eskandar@gmail.com>
2021-03-06 18:30:26 +0330

    accreprec tested and fixed
    there were some technical issues in implementation that fixed

--- accrecprec.py
+++ accrecprec.py
@@ -3,6 +3,13 @@ def acc_calc(y_pred, y_test, smoothing=0):
     tn = sum([y_pred[i] == -1 and y_pred[i] == y_test[i] for i in range(len(y_pred))]) + smoothing
     fp = sum([y_pred[i] == 1 and y_pred[i] != y_test[i] for i in range(len(y_pred))]) + smoothing
     fn = sum([y_pred[i] == -1 and y_pred[i] != y_test[i] for i in range(len(y_pred))]) + smoothing
+    acc = (tp + tn) / (len(y_pred))
+    prec_c1 = (tp) / (tp + fp) if (tp + fp) else 0
+    rec_c1 = (tp) / (tp + fn) if (tp + fn) else 0
+    prec_cm1 = (tn) / (tn + fn) if (tn + fn) else 0
+    rec_cm1 = (tn) / (tn + fp) if (tn + fp) else 0
+    f1_c1 = 2 * prec_c1 * rec_c1 / (prec_c1 + rec_c1) if (prec_c1 + rec_c1) else 0
+    f1_cm1 = 2 * prec_cm1 * rec_cm1 / (prec_cm1 + rec_cm1) if (prec_cm1 + rec_cm1) else 0
     print("--------------")
     print('accuracy is', acc)
     print('if view=1 is considered to be the positive class: precision>', prec_c1, '- recall>', rec_c1, '- f1>', f1_c1)
