a27df6cd22d757617a1a2b869577ea998e5740bf
parsa <parsa.eskandar@gmail.com>
2021-03-06 17:28:31 +0330

    Process data
    Process data in a specific form that can be used with rf function

--- random_forest.py
+++ random_forest.py
@@ -8,3 +8,10 @@ from sklearn.metrics import precision_recall_fscore_support
 def random_forest(mode="combined"):
     path_src = str(pathlib.Path().absolute())
     df_train, vocabulary = read_train_data(path_src)
+    df_train[mode] = df_train[mode].apply(lambda x: sparce_matrix_to_list(x))
+    arr_train = []
+    for item in df_train[mode]:
+        for elem in item:
+            arr_train.append(elem)
+
+    array = np.array(arr_train)
